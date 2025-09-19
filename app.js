ff8dbfe7040e9c6614194c70939dc35aa763320a
Marcelo Freixo <msfreixo70@gmail.com>
2025-09-19 08:19:35 -0300

    evitando nomes duplicados e adicionando botão sortear amigos

--- app.js
+++ app.js
@@ -6,11 +6,23 @@ let amigos = [];
     return;
     }
     amigos.push(nome);
-    let lista = document.getElementById("listaAmigos");
-    let item = document.createElement("li");
-    item.textContent = nome;
-    lista.appendChild(item);
-    document.getElementById("adicionarAmigos").value = "";
+    renderizarLista();
+    document.getElementById("nomeAmigo").value = "";
     }
+    function renderizarLista() {
+      let lista = document.getElementById("listaAmigos");
+      lista.innerHTML = "";
+      for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+      }
+    }
+    function sortearAmigo() {
+        if (amigos.length === 0) {
+        alert("A lista está vazia. Adicione amigos antes de sortear.");
+        return;
+      }
 
-
+      let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+      let amigoSorteado = amigos[indiceAleatorio];
+      document.getElementById("resultado").innerHTML = `🎉 O amigo sorteado foi: <strong>${amigoSorteado}</strong>`;
+    }
